9371e562e99df8c865b2ec3554e461b95e552d86
Jake Mitchell <dromazing@gmail.com>
2016-12-27 22:06:14 +0000

    Initial Commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,28 @@
  
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+
+var filename = 'flattwopointoh';
+var distDir = './dist';
+var unminifiedDir = './debug';
 
 gulp.task('sass', function () {
-  return gulp.src('./src/flattwopointoh.scss')
+  return gulp.src('./src/' + filename + '.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('min', function () {
-  return gulp.src('./src/flattwopointoh.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/flattwopointoh.scss', ['sass']);
+    .pipe(sourcemaps.write(unminifiedDir + '/maps'))
+    .pipe(gulp.dest(unminifiedDir));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./src/flattwopointoh.scss', ['min']);
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
+
+gulp.task('dist', function () {
+  return gulp.src('./src/' + filename + '.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename(filename + '.min.css'))
+    .pipe(gulp.dest(distDir));
 });
